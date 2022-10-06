9b5795cc80cb19dc4e0a75b20e24593bc39290ea
dzhavaharnal <112697031+dzhavaharnal@users.noreply.github.com>
2022-10-06 12:06:34 +0300

    Update 01_task_algo.py

--- Module10/practice/algo/01_task_algo.py
+++ Module10/practice/algo/01_task_algo.py
@@ -4,3 +4,18 @@
 # Вводится число N, далее еще N − 1 чисел: номера оставшихся карточек (различные числа от 1 до N).
 # Программа должна вывести номер потерянной карточки.
 
+n = int(input("N: "))
+numbers = []
+for _ in range(n - 1):
+    numbers.append(int(input("number: ")))
+
+numbers.sort(reverse=True)
+
+next_number = n + 1
+for number in numbers:
+    if next_number - number == 2:
+        print(number + 1)
+        break
+    next_number = number
+else:
+    print(1)
